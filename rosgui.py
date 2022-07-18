fc43b399fa452937aa07398d50d9216d6aeb2d36
Bradley Rauscher <bcraus@bu.edu>
2022-07-18 17:22:47 -0400

    New DLC gui

--- rosgui.py
+++ rosgui.py
@@ -21,6 +21,9 @@ class Window(ptw.QMainWindow):
     def __init__(self, parent = None):
         super(Window, self).__init__(parent)
         
+        styleComboBox = ptw.QComboBox()
+        styleComboBox.addItems(ptw.QStyleFactory.keys())
+        
         widget = ptw.QWidget()
         
         choosedir = ptw.QPushButton('Directory')
@@ -34,6 +37,7 @@ class Window(ptw.QMainWindow):
         self.createbotleftgb()
         self.createtoprightgb()
         self.createbotrightgb()
+        self.createprogressbar()
         
         toplayout = ptw.QHBoxLayout()
         toplayout.addWidget(choosedir)
@@ -46,12 +50,14 @@ class Window(ptw.QMainWindow):
         mainlayout.addWidget(self.botleftgb,2,0)
         mainlayout.addWidget(self.toprightgb,1,1)
         mainlayout.addWidget(self.botrightgb,2,1)
+        mainlayout.addWidget(self.progressbar,3,0,1,2)
         widget.setLayout(mainlayout)
         
         self.setCentralWidget(widget)
         
         self.setWindowTitle = "DLC Operator"
-        #self.setGeometry(xoffset,yoffset,winwidth,winheight) #x,y,w,h
+        self.setGeometry(xoffset,yoffset,winwidth,winheight) #x,y,w,h
+        
         
     def createtopleftgb(self):
         self.topleftgb = ptw.QGroupBox("Group 1")
@@ -62,21 +68,44 @@ class Window(ptw.QMainWindow):
         ex2.setDefault(True)
         ex3 = ptw.QPushButton('ex3')
         ex3.setDefault(True)
+        ex4 = ptw.QPushButton('ex4')
+        ex4.setDefault(True)
         
         layout = ptw.QVBoxLayout()
         layout.addWidget(ex1)
         layout.addWidget(ex2)
         layout.addWidget(ex3)
+        layout.addWidget(ex4)
         self.topleftgb.setLayout(layout)
     
     def createbotleftgb(self):
         self.botleftgb = ptw.QGroupBox("Group 2")
-    
+        
+        ex1 = ptw.QPushButton('ex1')
+        ex1.setDefault(True)
+        ex2 = ptw.QPushButton('ex2')
+        ex2.setDefault(True)
+        ex3 = ptw.QPushButton('ex3')
+        ex3.setDefault(True)
+        ex4 = ptw.QLabel('')
+        
+        layout = ptw.QVBoxLayout()
+        layout.addWidget(ex1)
+        layout.addWidget(ex2)
+        layout.addWidget(ex3)
+        layout.addWidget(ex4)
+        self.botleftgb.setLayout(layout)
+        
     def createtoprightgb(self):
         self.toprightgb = ptw.QGroupBox("Group 3")
     
     def createbotrightgb(self):
         self.botrightgb = ptw.QGroupBox("Group 4")
+    
+    def createprogressbar(self):
+        self.progressbar = ptw.QProgressBar()
+        self.progressbar.setRange(0, 10)
+        self.progressbar.setValue(0)
     
     def onClicked(self):
         radioButton = self.sender()
@@ -106,6 +135,8 @@ class Window(ptw.QMainWindow):
         subprocess.Popen(["open",file])
 
 app = ptw.QApplication(sys.argv)
+app.setStyle('macintosh')
+ptw.QApplication.setPalette(ptw.QApplication.style().standardPalette())
 
 screen = Window()
 screen.show()
