85c01fe10717e18bd3082c54f6611a7cb655e3fd
Bradley Rauscher <bcraus@bu.edu>
2022-08-18 20:37:51 -0400

    new dlc gui

--- tk_dlc_gui.py
+++ tk_dlc_gui.py
@@ -45,13 +45,107 @@ class ProcessRunnable(QtCore.QRunnable):
     def start(self):
         QtCore.QThreadPool.globalInstance().start(self)
 
-def run(log):
-    print('hello')
-    log.progressbar['value'] = 100
-    log.curdir = '/'
-    time.sleep(5)
-    print('done')
+def getparentex(file):
+    pos = []
+    pos.append(file.find('/',0,len(file)))
+    while pos[-1] != -1:
+        pos.append(file.find('/',pos[-1]+1,len(file)))
+    file = file[:pos[-2]]
+    return(file)
 
+def run(log):
+    log[0].progressbar['value'] = 0
+    progint = 0
+    for frame in (log[0].topleft,log[0].botleft,log[0].topright):
+        for child in frame.winfo_children():
+            child.configure(state='disable')
+            
+    try:
+        if log[1] == 'Create Project':
+            #dlc.create_new_project(log[1].projname,log[1].initials,log[1].vidpaths, working_directory=log[1].curdir,copy_videos=False)
+            time.sleep(0.5)
+            
+            pass
+        
+        elif log[1] == 'Add Videos':
+            #dlc.add_new_videos(log[1].configfile,log[1].vidpaths,copy_videos=False)
+            pass
+        
+        elif log[1] == 'Extract Frames':
+            #dlc.extract_frames(log[1].configfile,"manual",crop=False, userfeedback=False)
+            pass
+        
+        elif log[1] == 'Label Frames':
+            #dlc.label_frames(log[1].configfile)
+            pass
+        
+        elif log[1] == 'Create Training Dataset':
+            #dlc.create_training_dataset(log[1].configfile,num_shuffles=1)
+            pass
+        
+        elif log[1] == 'Train Network':
+            #dlc.train_network(log[1].configfile,maxiters=log[1].maxiters)
+            pass
+        
+        elif log[1] == 'Evaluate Network':
+            #dlc.evaluate_network(log[1].configfile,plotting=True)
+            pass
+        
+        elif log[1] == 'Analyze Label':
+            log[0].cleargrid()
+            
+            ####
+            log[0].progressbar['maximum'] = len(log[0].vidpaths)
+            
+
+            for vid in log[0].vidpaths:
+                #dlc.analyze_videos(log[1].configfile,vid,shuffle=1,save_as_csv=True)
+                #dlc.create_labeled_video(log[1].configfile,vid,draw_skeleton=True)
+                progint += 1
+                time.sleep(1)
+                #finds new video
+                parent = getparentex(vid)
+                
+                files = []
+                mp4files = []
+                
+                for (dirpath, dirnames, filenames) in os.walk(parent):
+                    files.extend(filenames)
+                    break
+                for vidname in files:
+                    if vidname[-3:] == '.py':
+                        fullpath = dirpath+'/'+vidname
+                        mp4files.append(fullpath)
+                    else:
+                        pass
+                
+                newvid = max(mp4files,key=len)
+                
+                log[0].vidgrid.insert('',tk.END, values=newvid)
+                
+                log[0].progressbar['value'] = progint
+            
+        
+        elif log[1] == 'Extract Outliers':
+            #dlc.extract_outlier_frames(log[1].configfile,log[1].vidpaths)
+            pass
+        
+        elif log[1] == 'Refine Labels':
+            #dlc.refine_labels(log[1].configfile)
+            pass
+        
+        elif log[1] == 'Merge Datasets':
+            #dlc.merge_datasets(log[1].configfile)
+            pass
+    except:
+        pass
+    
+    log[0].progressbar['value'] = log[0].progressbar['maximum']
+    for frame in (log[0].topleft,log[0].botleft,log[0].topright):
+        for child in frame.winfo_children():
+            child.configure(state='normal')
+    
+    
 class MyWindow:
     def __init__(self,win):
         
@@ -123,16 +217,16 @@ class MyWindow:
         self.topleft = tk.Frame(inp,bg=frame_color)
         cont = self.topleft
         
-        w1 = ttk.Button(cont,text="Create Project", style="big.TButton")
+        w1 = ttk.Button(cont,text="Create Project", style="big.TButton", command=(lambda: self.DLCcommands((self,'Create Project'))))
         w1.grid(row=0,column=0,padx=butpadding,sticky='WE')
         
-        w2 = ttk.Button(cont,text="Add Videos", style="big.TButton")
+        w2 = ttk.Button(cont,text="Add Videos", style="big.TButton", command=(lambda: self.DLCcommands((self,'Add Videos'))))
         w2.grid(row=1,column=0,padx=butpadding,sticky='WE')
         
-        w3 = ttk.Button(cont,text="Extract Frames", style="big.TButton")
+        w3 = ttk.Button(cont,text="Extract Frames", style="big.TButton", command=(lambda: self.DLCcommands((self,'Extract Frames'))))
         w3.grid(row=2,column=0,padx=butpadding,sticky='WE')
         
-        w4 = ttk.Button(cont,text="Label Frames", style="big.TButton")
+        w4 = ttk.Button(cont,text="Label Frames", style="big.TButton", command=(lambda: self.DLCcommands((self,'Label Frames'))))
         w4.grid(row=3,column=0,padx=butpadding,sticky='WE')
         
         cont.grid_columnconfigure(0,weight=1)
@@ -146,16 +240,16 @@ class MyWindow:
         self.topright = tk.Frame(inp,bg=frame_color)
         cont = self.topright
         
-        w1 = ttk.Button(cont,text="Analyze/Label Videos", style="big.TButton", command=(lambda: self.backproc(self)))
+        w1 = ttk.Button(cont,text="Analyze/Label Videos", style="big.TButton", command=(lambda: self.DLCcommands((self,'Analyze Label'))))
         w1.grid(row=0,column=0,padx=butpadding,sticky='WE')
         
-        w2 = ttk.Button(cont,text="Extract Outliers", style="big.TButton", command=self.progress)
+        w2 = ttk.Button(cont,text="Extract Outliers", style="big.TButton", command=(lambda: self.DLCcommands((self,'Extract Outliers'))))
         w2.grid(row=1,column=0,padx=butpadding,sticky='WE')
         
-        w3 = ttk.Button(cont,text="Refine Labels", style="big.TButton", command=self.reset)
+        w3 = ttk.Button(cont,text="Refine Labels", style="big.TButton", command=(lambda: self.DLCcommands((self,'Refine Labels'))))
         w3.grid(row=2,column=0,padx=butpadding,sticky='WE')
         
-        w4 = ttk.Button(cont,text="Merge Datasets", style="big.TButton", command=self.cleargrid)
+        w4 = ttk.Button(cont,text="Merge Datasets", style="big.TButton", command=(lambda: self.DLCcommands((self,'Merge Datasets'))))
         w4.grid(row=3,column=0,padx=butpadding,sticky='WE')
         
         cont.grid_columnconfigure(0,weight=1)
@@ -169,13 +263,13 @@ class MyWindow:
         self.botleft = tk.Frame(inp,bg=frame_color)
         cont = self.botleft
         
-        w1 = ttk.Button(cont,text="Create Training Dataset", style="big.TButton")
+        w1 = ttk.Button(cont,text="Create Training Dataset", style="big.TButton", command=(lambda: self.DLCcommands((self,'Create Training Dataset'))))
         w1.grid(row=0,column=0,padx=butpadding,sticky='WE')
         
-        w2 = ttk.Button(cont,text="Train Network", style="big.TButton")
+        w2 = ttk.Button(cont,text="Train Network", style="big.TButton", command=(lambda: self.trainnet((self,'Train Network'))))
         w2.grid(row=1,column=0,padx=butpadding,sticky='WE')
         
-        w3 = ttk.Button(cont,text="Evaluate Network", style="big.TButton")
+        w3 = ttk.Button(cont,text="Evaluate Network", style="big.TButton", command=(lambda: self.DLCcommands((self,'Evaluate Network'))))
         w3.grid(row=2,column=0,padx=butpadding,sticky='WE')
         
         w4 = tk.Label(cont)
@@ -338,17 +432,6 @@ class MyWindow:
         subprocess.Popen(["open",link])
     
     #__DLC Buttons__
-    
-    def backproc(self,bar):
-        self.p = ProcessRunnable(target=run, args=(bar))
-        self.p.start()
-    
-    def progress(self):
-        self.progressbar['maximum'] = 160
-        self.progressbar.step(20)
-        
-    def reset(self):
-        self.progressbar['value'] = 0
         
     def cleargrid(self):
         self.vidgrid.delete(*self.vidgrid.get_children())
@@ -356,6 +439,21 @@ class MyWindow:
     def opengridlink(self,event):
         link = self.vidgrid.item(self.vidgrid.focus())['values']
         subprocess.Popen(["open",link[0]])
+    
+    def DLCcommands(self,dlcarg):
+        self.p = ProcessRunnable(target=run, args=(dlcarg))
+        self.p.start()
+    
+    def trainnet(self,dlcarg):
+        self.maxiters = askinteger('Train Network',"""Maxiters? (typically "200000")""")
+        self.p = ProcessRunnable(target=run, args=(dlcarg))
+        self.p.start()
+    
+    def createnet(self,dlcarg):
+        self.projname = ('Create New Project','Title of Project?')
+        self.initials = askstring('Create New Project','Creator Initials?')
+        self.p = ProcessRunnable(target=run, args=(dlcarg))
+        self.p.start()
         
     
 window = tk.Tk()
